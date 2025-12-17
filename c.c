ba026a1845258d4f1a0829e1c522e6d4eedf7ca7
Krapivin Vadim <krapivinvadim4@gamil.com>
2025-12-17 16:47:50 +0300

    It works (shity, but works)

--- c.c
+++ c.c
@@ -2,25 +2,12 @@
 #include "avr/io.h"
 #include <stdio.h>
 #include "avr/interrupt.h"
-#include "util/delay.h"
 #include "UART.h"
 #include "SPI.h"
 #include <stdlib.h>
-
-
-// void INT_init() {
-
-//     sei();
-//     DDRD &= ~(1 << 2); // -----0--
-//     PORTD |= (1 << 2); // -----1--
-//     EICRA |= (1 << ISC01);
-//     EIMSK |= (1 << INT0);
-//     UCSR0B |= (1 << RXCIE0);
-
-// }
+#include <math.h>
 
 void ADC_init() {
-    // ADMUX |= (1 << REFS0);
     ADCSRA |= (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1);
 
 }
@@ -29,27 +16,25 @@ uint16_t ADC_conversion(uint8_t channel) {
 
     ADMUX = (ADMUX & 0xF0) | (channel & 0x0F);
     ADCSRA |= (1 << ADSC);
-    while (!(ADCSRA & (1 << ADIF))); // пока в ADIF 0 (или 1, я хз) ждём прерывание
+    while (!(ADCSRA & (1 << ADIF)));
     ADCSRA |= (1 << ADIF);
     return ADC;
 
 }
 
 UART* UART_ = NULL;
+unsigned char current_level = 15;
 volatile uint8_t isADC_conv = 0;
 
-uint8_t FROM_UART;
-
 ISR(USART_RX_vect) {
-    // FROM_UART = USART_Receive();
-    // USART_Transmit(FROM_UART);
     UART_->USART_Receive(UART_);
-    UART_->USART_Transmit(UART_, '>');
-    UART_->USART_Transmit(UART_, UART_->char_buffer);
-    UART_->USART_Transmit(UART_, '\n');
-    UART_->USART_Transmit(UART_, '\r');
+    uint8_t h = UART_->char_buffer - 48;
+    UART_->USART_Receive(UART_);
+    uint8_t d = UART_->char_buffer - 48;
+    UART_->USART_Receive(UART_);
+    uint8_t o = UART_->char_buffer - 48;;
+    current_level = h * 100 + d * 10 + o;
 }
-
 
 
 int main() {
@@ -58,24 +43,33 @@ int main() {
     UART_ = uart_constructor();
     UART_->uart_init(UART_);
     UART_->INT_init(UART_);
-    UART_->char_buffer = (char) 0;
+    UART_->char_buffer = 15;
+    unsigned char resistance = 1;
     ADC_init();
     char data[LEN_SIZE];
-    uint8_t i = 0;
     while (1) {
-        spi->transmit(spi, (char)UART_->char_buffer);
-        char* output = "CURRENT: %d.%03d\n\r";
+        
+        char* output = "CURRENT: %d TARGET: %d\n\r";
         uint32_t ADC_meas = (uint32_t)ADC_conversion(6) * 5000 / 1024  / 100;
-        // ADC_meas = ADC_meas / 750;
-        uint16_t ADC_calc = ADC_meas / 1000;
-        uint16_t ADC_dec = ADC_meas % 1000;
-        uint8_t len = snprintf(data, LEN_SIZE, output, ADC_meas, 0);
+        uint8_t len = snprintf(data, LEN_SIZE, output, ADC_meas, (uint8_t)current_level);
+        if(ADC_meas > current_level){
+            unsigned char delta = 1; //pow(ADC_meas - current_level, 2);
+            unsigned char new_resistance = resistance + delta;
+            if (resistance > (255 - delta)){
+                new_resistance = 255;
+            }
+            resistance = new_resistance;
+        }
+        else if(current_level > ADC_meas){
+            unsigned char delta = 1; //pow(current_level - ADC_meas, 2);
+            unsigned char new_resistance = resistance - delta;
+            if(delta > resistance){
+                new_resistance = 0;
+            }
+            resistance = new_resistance;
+        }
+        spi->transmit(spi, resistance);
         UART_->USART_Transmit_str(UART_, data, len);
-        // uint8_t len = snprintf(data, LEN_SIZE, output, ADC_calc, ADC_dec);
-        // UART_->USART_Transmit_str(UART_, data, len);
-        ++i;
-        _delay_ms(100);
-        _delay_ms(100);
     }
     free(UART_);
     free(spi);
